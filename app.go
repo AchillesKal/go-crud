f5b6f4e94a454c422ba9329fd2d8ef696e2aef3c
Achilles Kaloeridis <achilleas90gr@gmail.com>
2019-03-14 21:25:22 +0200

    Refactor crud

--- app.go
+++ app.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"gocart/router"
 	"html/template"
 	"log"
 	"net/http"
-	"net/url"
 
+	router "github.com/AchillesKal/go-router"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -31,195 +29,16 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
-type Controller interface {
-	run(w http.ResponseWriter, r *http.Request)
-}
-
-type Home struct {
-}
-
-func (h *Home) run(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	selDB, err := db.Query("SELECT * FROM product ORDER BY id ASC")
-	if err != nil {
-		panic(err.Error())
-	}
-	product := Product{}
-	res := []Product{}
-	for selDB.Next() {
-		var id int
-		var name string
-		err = selDB.Scan(&id, &name)
-		if err != nil {
-			panic(err.Error())
-		}
-		product.Id = id // cart.Sum(1, 5)
-		product.Name = name
-		res = append(res, product)
-	}
-	tmpl.ExecuteTemplate(w, "Index", res)
-	defer db.Close()
-}
-
-type Show struct {
-}
-
-func (s *Show) run(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	nId := r.URL.Query().Get("id")friends/requests/?fcref=ffi
-	selDB, err := db.Query("SELECT * FROM product WHERE id=?", nId)
-	if err != nil {
-		panic(err.Error())
-	}
-	product := Product{}
-	for selDB.Next() {
-		var id int
-		var name string
-		err = selDB.Scan(&id, &name)
-		if err != nil {
-			panic(err.Error())
-		}
-		product.Id = id
-		product.Name = name
-	}
-	tmpl.ExecuteTemplate(w, "Show", product)
-	defer db.Close()
-}
-
-type CreateNew struct {
-}
-
-func (n *CreateNew) run(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "New", nil)
-}
-
-type Edit struct {
-}
-
-func (e *Edit) run(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM product WHERE id=?", nId)
-	if err != nil {
-		panic(err.Error())
-	}
-	product := Product{}
-	for selDB.Next() {
-		var id int
-		var name string
-		err = selDB.Scan(&id, &name)
-		if err != nil {
-			panic(err.Error())
-		}
-		product.Id = id
-		product.Name = name
-	}
-	tmpl.ExecuteTemplate(w, "Edit", product)
-	defer db.Close()
-}
-
-type Insert struct {
-}
-
-func (i *Insert) run(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	if r.Method == "POST" {
-		name := r.FormValue("name")
-		insForm, err := db.Prepare("INSERT INTO product(name) VALUES(?)")
-		if err != nil {
-			panic(err.Error())
-		}
-		insForm.Exec(name)
-		log.Println("INSERT: Name: " + name)
-	}
-	defer db.Close()
-	http.Redirect(w, r, "/", 301)
-}
-
-type Update struct {
-}
-
-func (u *Update) run(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	if r.Method == "POST" {
-		name := r.FormValue("name")
-		id := r.FormValue("uid")
-		insForm, err := db.Prepare("UPDATE product SET name=? WHERE id=?")
-		if err != nil {
-			panic(err.Error())
-		}
-		insForm.Exec(name, id)
-		log.Println("UPDATE: Name: " + name)
-	}
-	defer db.Close()
-	http.Redirect(w, r, "/", 301)
-}
-
-type Delete struct{}
-
-func (d *Delete) run(w http.ResponseWriter, r *http.Request) {
-	log.Println("pre DELETE")
-	db := dbConn()
-	product := r.URL.Query().Get("id")
-	delForm, err := db.Prepare("DELETE FROM product WHERE id=?")
-	if err != nil {
-		panic(err.Error())
-	}
-	delForm.Exec(product)
-	log.Println("DELETE")
-	defer db.Close()
-	http.Redirect(w, r, "/", 301)
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	requestPath := r.URL.Path
-	home := new(Home)
-	show := new(Show)
-	cnew := new(CreateNew)
-	edit := new(Edit)
-	insert := new(Insert)
-	update := new(Update)
-	delete := new(Delete)
-
-	routes := map[string]Controller{
-		"/":       home,
-		"/show":   show,
-		"/new":    cnew,
-		"/edit":   edit,
-		"/insert": insert,
-		"/update": update,
-		"/delete": delete,
-	}
-
-	for path, controller := range routes {
-		if path == requestPath {
-			controller.run(w, r)
-			return
-		}
-	}
-
-	w.WriteHeader(404)
-	fmt.Fprint(w, "custom 404")
-	return
-}
-
 func main() {
-
-	r := router.New(Root)
-
+	appRouter := router.New(index)
 	log.Println("Server started on: http://localhost:8080")
-	// http.HandleFunc("/", Index)
-	r.Handle("GET", "/", Root)
-	r.Handle("GET", "/users/:name", UserShow)
+	appRouter.Handle("GET", "/", index)
+	appRouter.Handle("GET", "/show/:id", showProduct)
+	appRouter.Handle("GET", "/new", newProduct)
+	appRouter.Handle("GET", "/edit/:id", editProduct)
+	appRouter.Handle("POST", "/insert", insertProduct)
+	appRouter.Handle("POST", "/update", updateProduct)
+	appRouter.Handle("GET", "/delete/:id", deleteProduct)
 
-	http.ListenAndServe(":8080", r)
-
-}
-
-func Root(w http.ResponseWriter, r *http.Request, params url.Values) {
-	fmt.Fprint(w, "Root!\n")
-}
-
-func UserShow(w http.ResponseWriter, r *http.Request, params url.Values) {
-	fmt.Fprintf(w, "Hi %s", params["name"])
+	http.ListenAndServe(":8080", appRouter)
 }
--- routes.go
+++ routes.go
@@ -1,22 +1,125 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 )
 
-func (s *server) routes() {
-	s.router.HandleFunc("/about", s.handleAbout())
-	s.router.HandleFunc("/", s.handleIndex())
-}
-
-func (s *server) handleAbout() http.HandlerFunc {
-	thing := prepareThing()
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "About")
+func index(w http.ResponseWriter, r *http.Request, params url.Values) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM product ORDER BY id ASC")
+	if err != nil {
+		panic(err.Error())
 	}
+	product := Product{}
+	res := []Product{}
+	for selDB.Next() {
+		var id int
+		var name string
+		err = selDB.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		product.Id = id
+		product.Name = name
+		res = append(res, product)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
 }
 
-func prepareThing() string {
-	return "coo"
+func showProduct(w http.ResponseWriter, r *http.Request, params url.Values) {
+	db := dbConn()
+	nId := params["id"][0]
+
+	selDB, err := db.Query("SELECT * FROM product WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	product := Product{}
+	for selDB.Next() {
+		var id int
+		var name string
+		err = selDB.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		product.Id = id
+		product.Name = name
+	}
+	tmpl.ExecuteTemplate(w, "Show", product)
+	defer db.Close()
+}
+
+func newProduct(w http.ResponseWriter, r *http.Request, params url.Values) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+func editProduct(w http.ResponseWriter, r *http.Request, params url.Values) {
+	db := dbConn()
+	nId := params["id"][0]
+	selDB, err := db.Query("SELECT * FROM product WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	product := Product{}
+	for selDB.Next() {
+		var id int
+		var name string
+		err = selDB.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		product.Id = id
+		product.Name = name
+	}
+	tmpl.ExecuteTemplate(w, "Edit", product)
+	defer db.Close()
+}
+
+func insertProduct(w http.ResponseWriter, r *http.Request, params url.Values) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		insForm, err := db.Prepare("INSERT INTO product(name) VALUES(?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name)
+		log.Println("INSERT: Name: " + name)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request, params url.Values) {
+	db := dbConn()
+	log.Println("UPDATE: method: " + r.Method)
+
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		id := r.FormValue("uid")
+		insForm, err := db.Prepare("UPDATE product SET name=? WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, id)
+		log.Println("UPDATE: Name: " + name)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request, params url.Values) {
+	db := dbConn()
+	nId := params["id"][0]
+	delForm, err := db.Prepare("DELETE FROM product WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	delForm.Exec(nId)
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
 }
