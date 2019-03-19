2f1b26231df8d4b002c37030559b5a3219014308
Achilles Kaloeridis <Achilleas90gr@gmail.com>
2019-03-19 22:08:21 +0200

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS gocrud_db;
 
-CREATE TABLE gocrud_db.product (
+CREATE TABLE IF NOT EXISTS gocrud_db.product (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255),
     PRIMARY KEY (id)
